8d6db3d16a7d97a50443ab0d1ac77bab82550097
gman <anisimovf94@gmail.com>
2024-03-22 17:19:54 +0300

    Adds the Shape Calculator class to calculate the area of shapes

---
+++ area/shape_calculator.py
@@ -0,0 +1,31 @@
+import math
+
+
+class ShapeCalculator:
+    @staticmethod
+    def get_circle_area(radius: float):
+        """Calculating the area of a square"""
+        return math.pi * radius ** 2
+
+    @staticmethod
+    def get_triangle_area(side1: float, side2: float, side3: float):
+        """Calculating the area of a triangle"""
+        s = (side1 + side2 + side3) / 2
+        return math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
+
+    @staticmethod
+    def is_right_triangle(side1: float, side2: float, side3: float):
+        """Checking whether the triangle is rectangular"""
+        sides = [side1, side2, side3]
+        sides.sort()
+        return sides[0] ** 2 + sides[1] ** 2 == sides[2] ** 2
+
+    @staticmethod
+    def get_area(*args):
+        """Calculates the area of a shape without knowing the type of shape in compile-time."""
+        if len(args) == 1:
+            return ShapeCalculator.get_circle_area(args[0])
+        elif len(args) == 3:
+            return ShapeCalculator.get_triangle_area(*args)
+        else:
+            raise ValueError("unidentified form")
---
+++ tests/test.py
@@ -0,0 +1,30 @@
+import math
+import unittest
+from area import ShapeCalculator
+
+
+class TestGeometry(unittest.TestCase):
+    def test_get_circle_area(self):
+        self.assertAlmostEqual(ShapeCalculator.get_circle_area(1), math.pi)
+        self.assertAlmostEqual(ShapeCalculator.get_circle_area(0), 0)
+        self.assertAlmostEqual(ShapeCalculator.get_circle_area(5), 25 * math.pi)
+
+    def test_get_triangle_area(self):
+        self.assertAlmostEqual(ShapeCalculator.get_triangle_area(3, 4, 5), 6)
+        self.assertAlmostEqual(ShapeCalculator.get_triangle_area(6, 8, 10), 24)
+        self.assertAlmostEqual(ShapeCalculator.get_triangle_area(5, 5, 5), 10.825317547305485)
+
+    def test_get_area(self):
+        self.assertAlmostEqual(ShapeCalculator.get_area(3, 4, 5), 6)
+        self.assertAlmostEqual(ShapeCalculator.get_area(6, 8, 10), 24)
+        self.assertAlmostEqual(ShapeCalculator.get_area(1), math.pi)
+        self.assertAlmostEqual(ShapeCalculator.get_area(0), 0)
+
+    def test_get_unknown_shape(self):
+        with self.assertRaises(ValueError):
+            ShapeCalculator.get_area(1, 2)
+            ShapeCalculator.get_area(1, 2, 3, 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
